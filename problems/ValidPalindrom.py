847a9e56cbcad29b6bbe0fe6aff3df585681cce5
Rishabh Sharma <59526954+ris3abh@users.noreply.github.com>
2025-03-13 15:04:36 -0400

    new codes and updates

--- problems/ValidPalindrom.py
+++ problems/ValidPalindrom.py
@@ -1,14 +1,49 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        if len(s) < 2:          return True
-        h, t = 0, len(s)-1
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+# Example 2:
+
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+# Example 3:
+
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 2 * 105
+# s consists only of printable ASCII characters.
+
+def isPalindrome(text):
+        if len(text) < 2:
+            return True
+        h, t = 0, len(text) - 1
         while h < t:
-            if not s[h].isalnum(): h += 1
-            elif not s[t].isalnum():t -= 1
+            if not text[h].isalnum():
+                h+=1
+            elif not text[t].isalnum():
+                t-=1
             else:
-                if s[h].lower() == s[t].lower():
-                    h += 1
-                    t -= 1
-                else:
-                    return False
+                 if text[h].lower() != text[t].lower():
+                      return False
+                 h+=1
+                 t-=1
         return True
+
+
+text = "A man, a plan, a canal: Panama"
+output = isPalindrome(text)
+print(output)
